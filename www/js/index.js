275578d009174d9565967a6b21988be639ce4dd0
Tai Le <vzttzv@gmail.com>
2019-02-14 12:08:54 +0700

    db stuff

--- www/js/index.js
+++ www/js/index.js
@@ -1,11 +1,57 @@
+const sql_createTables = `
+  CREATE TABLE Storage_StorageFeatures
+  (
+    Storage_Id         INTEGER NOT NULL,
+    StorageFeatures_Id INTEGER NOT NULL,
+    PRIMARY KEY (Storage_Id,
+                 StorageFeatures_Id),
+    FOREIGN KEY (Storage_Id) REFERENCES Storages (Id),
+    FOREIGN KEY (StorageFeatures_Id) REFERENCES StorageFeatures (Id)
+  );
+  CREATE TABLE StorageFeatures
+  (
+    Id   INTEGER      NOT NULL PRIMARY KEY AUTOINCREMENT,
+    Name TEXT NOT NULL
+  );
+  CREATE TABLE Storages
+  (
+    Id             INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT,
+    StorageType_Id INTEGER  NOT NULL,
+    "size"         REAL     NOT NULL,
+    datetime_added TEXT NOT NULL,
+    rent_price     REAL     NOT NULL,
+    notes          TEXT,
+    reporter_name  TEXT NOT NULL,
+    FOREIGN KEY (StorageType_Id) REFERENCES StorageTypes (Id)
+  );
+  CREATE TABLE StorageTypes
+  (
+    Id   INTEGER      NOT NULL PRIMARY KEY AUTOINCREMENT,
+    Name TEXT NOT NULL
+  );
+`;
+
 var app = {
+    db: null,
     // Application Constructor
-    initialize: function() {
+    initialize: function () {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
-    
-    onDeviceReady: function() {
-        
+
+    onDeviceReady: function () {
+        function prepareDb() {
+            this.db = window.openDatabase("mystoragedb", "1.0", "Storages DB", 2 * 1024 * 1024);
+
+            this.db.transaction((tx) => {
+                tx.executeSql(sql_createTables, null, function (tx, result) {
+                    console.log(result);
+                }, function (tx, error) {
+                    console.log(error);
+                });
+            })
+        }
+
+        prepareDb();
     }
 };
 
