d3800eb9b4e54dea6a4a2881144cf18ebd14887f
Unknown <vzttzv@gmail.com>
2019-02-21 11:08:31 +0700

    basic input

--- android/app/src/main/java/com/letai/mystorage/MainActivity.java
+++ android/app/src/main/java/com/letai/mystorage/MainActivity.java
@@ -9,8 +9,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener
 {
 
     @Override
@@ -20,6 +24,33 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Spinner storageTypeSpinner = findViewById(R.id.input_storage_type);
+
+        storageTypeSpinner.setOnItemSelectedListener(this);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                new String[]{
+                        "Home",
+                        "Business"
+                });
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        storageTypeSpinner.setAdapter(adapter);
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l)
+    {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView)
+    {
+
+    }
 }
