f0bd9f0fa891925e46653ef3616879593a94e6a1
Tai Le <vzttzv@gmail.com>
2019-02-26 11:58:49 +0700

    WIP db insert

--- www/js/dbmanager.js
+++ www/js/dbmanager.js
@@ -5,15 +5,46 @@ const Table = {
     Storage_StorageFeatures: "Storage_StorageFeatures",
 };
 
+const StorageFeature = {
+    PrivateSpace: 'Private Space',
+    SharingSpace: 'Sharing Space',
+    CCTV: 'CCTV'
+};
+
 const dbmanager = {
     db: null,
 
     initialize: function () {
         this.db = this.openDb();
         this.prepareTables(this.db);
+        this.fillData(this.db);
     },
-    
-    
+
+    saveFormData: function (formData) {
+        this.db.transaction(tx => {
+            tx.executeSql(`
+INSERT INTO ${Table.Storages} (StorageType_Name, "size", datetime_added, rent_price, notes, reporter_name)
+VALUES (
+        (SELECT name FROM ${Table.StorageTypes} WHERE name='${formData.storageTypes}'),
+        ${formData.size},
+        '${formData.timeAdded}',
+        ${formData.rentPrice},
+        '${formData.notes}',
+        '${formData.reporterName}'
+       )`
+            );
+        }, console.error)
+    },
+
+    fillData: function (db) {
+        db.transaction(tx => {
+            tx.executeSql(`INSERT into ${Table.StorageTypes} values ('Home'),('Business')`);
+            tx.executeSql(`INSERT into ${Table.StorageFeatures} values 
+                                     ('${StorageFeature.PrivateSpace}'),
+                                     ('${StorageFeature.SharingSpace}'),
+                                     ('${StorageFeature.CCTV}')`);
+        }, console.error)
+    },
 
     openDb: function () {
         return window.openDatabase(
@@ -25,42 +56,41 @@ const dbmanager = {
     },
 
     prepareTables: function (db) {
-        db.transaction(createTables, error => console.log(error));
+        db.transaction(createTables, console.error);
 
         function createTables(tx) {
             // language=SQLite
             tx.executeSql(`CREATE TABLE IF NOT EXISTS ${Table.StorageTypes}
                            (
-                             Id   INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                             Name TEXT    NOT NULL
+                              Name TEXT NOT NULL,
+                              PRIMARY KEY (Name)
                            )`);
             // language=SQLite
             tx.executeSql(`CREATE TABLE IF NOT EXISTS ${Table.Storages}
                            (
                              Id             INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                             StorageType_Id INTEGER NOT NULL,
+                             StorageType_Name TEXT NOT NULL,
                              "size"         REAL    NOT NULL,
                              datetime_added TEXT    NOT NULL,
                              rent_price     REAL    NOT NULL,
                              notes          TEXT,
                              reporter_name  TEXT    NOT NULL,
-                             FOREIGN KEY (StorageType_Id) REFERENCES ${Table.StorageTypes} (Id)
+                             FOREIGN KEY (StorageType_Name) REFERENCES ${Table.StorageTypes} (Name)
                            )`);
             // language=SQLite
             tx.executeSql(`CREATE TABLE IF NOT EXISTS ${Table.StorageFeatures}
                            (
-                             Id   INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                             Name TEXT    NOT NULL
+                             Name TEXT    NOT NULL,
+                             PRIMARY KEY (Name)
                            )`);
             // language=SQLite
             tx.executeSql(`CREATE TABLE IF NOT EXISTS ${Table.Storage_StorageFeatures}
                            (
-                             Storage_Id        INTEGER NOT NULL,
-                             StorageFeature_Id INTEGER NOT NULL,
-                             PRIMARY KEY (Storage_Id,
-                                          StorageFeature_Id),
-                             FOREIGN KEY (Storage_Id) REFERENCES ${Table.Storages} (Id),
-                             FOREIGN KEY (StorageFeature_Id) REFERENCES ${Table.StorageFeatures} (Id)
+                              Storage_Id           integer NOT NULL, 
+                              StorageFeatures_Name text NOT NULL, 
+                              PRIMARY KEY (Storage_Id, StorageFeatures_Name), 
+                              FOREIGN KEY(Storage_Id) REFERENCES Storages(Id), 
+                              FOREIGN KEY(StorageFeatures_Name) REFERENCES StorageFeatures(Name)
                            )`);
         }
     },
