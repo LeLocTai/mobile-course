ee2f624711ddaf79f6d83cf9b4e17f739433a7d1
Tai Le <vzttzv@gmail.com>
2019-02-26 11:58:49 +0700

    WIP db insert

--- platforms/android/app/src/main/assets/www/js/data-entry.js
+++ platforms/android/app/src/main/assets/www/js/data-entry.js
@@ -1,3 +1,29 @@
-document.getElementById("form").onsubmit = (e) => {
+const formDataTemplate = {
+    storageTypes: null,
+    size: null,
+    timeAdded: null,
+    features: [],
+    rentPrice: null,
+    notes: null,
+    reporterName: null
+};
+
+Object.freeze(formDataTemplate);
+
+const form = $('#form');
+
+form.submit(e => {
     e.preventDefault();
-}
+    let formData = Object.assign({}, formDataTemplate);
+    let rawData = form.serializeArray();
+
+    rawData.forEach(entry => {
+        if (entry.value === 'on' && StorageFeature.hasOwnProperty(entry.name)) {
+            formData.features.push(entry.name);
+        } else {
+            formData[entry.name] = entry.value;
+        }
+    });
+
+    dbmanager.saveFormData(formData);
+});
