6c85c5286efc05a801218169029ddb5e4ae46646
Tai Le <vzttzv@gmail.com>
2019-03-05 12:14:00 +0700

    organize pages

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,4 +1,5 @@
 const app = {
+    storageListView: null,
     // Application Constructor
     initialize: function () {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
@@ -6,7 +7,41 @@ const app = {
 
     onDeviceReady: function () {
         dbmanager.initialize();
+        this.storageListView = $('#storage-list');
+        this.updateStorageList();
     },
+
+    updateStorageList: function () {
+        this.storageListView.empty();
+
+        this.storageListView.append($(this.getStorageListItem(
+            null,
+            null,
+            null,
+            null,
+            null,
+            null
+        )));
+
+        this.storageListView.listview('refresh');
+    },
+
+    getStorageListItem: function (id, type, size, price, note, reporterName) {
+        return `
+<li>
+    <a href="data-entry.html">
+        <p><small>${id}</small></p>
+        <h2>$${price}/month</h2>
+        <p>${size} m²</p>        
+        ${note ? `<p>${note}</p>` : ''}
+        <section class="ui-li-aside">
+            <h2>${type}</h2>
+            <p>by ${reporterName}</p>
+        </section>
+    </a>
+</li>
+`;
+    }
 };
 
 $(document).on("mobileinit", function () {
@@ -14,4 +49,9 @@ $(document).on("mobileinit", function () {
     $.mobile.phonegapNavigationEnabled = true;
 });
 
+$(document).on( "pagecreate", function() {
+    
+});
+
 app.initialize();
+
