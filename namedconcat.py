21b57ea1be024daa742fd540de7a42818f08a0ec
Tai Le <vzttzv@gmail.com>
2019-03-26 16:35:48 +0700

    namedconcat

--- namedconcat.py
+++ namedconcat.py
@@ -4,29 +4,48 @@ import glob
 import os
 import sys
 
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("inDir")
+args = parser.parse_args()
+
+inDir = os.path.abspath(args.inDir)
+outPath = os.path.abspath(os.path.basename(inDir)+".txt")
+
 exts = ['js', 'css', 'html']
 
 
 def isText(path):
-    name, ext = os.path.splitext(path)
-    ext = ext[1:]
+    _, ext = os.path.splitext(path)
+    ext = ext[1:]  # remove dot
     return ext in exts
 
 
 def findRecursive(s):
-    return set(glob.glob(path+"/"+s, recursive=True))
-
-# if len(sys.argv) > 1:
-    # path = str(sys.argv[1])
-# else:
+    return set(glob.glob(inDir + "/" + s, recursive=True))
 
 
-path = os.getcwd()
+def writeOut(inPaths, outPath):
+    outfile = open(outPath, 'w')
+
+    for path in inPaths:
+        relpath = os.path.relpath(path, start=inDir)
+        print("Adding: " + relpath)        
+        outfile.write("//" + relpath + "\n\n")
+
+        inFile = open(path)
+        for line in inFile:
+            outfile.write(line)
+        inFile.close()
+
+        outfile.write('\n\n')
+
+    outfile.close()
+
 
 allPaths = findRecursive("/**/*.*")
 libPaths = findRecursive("/**/lib/**/*.*")
 
 scriptPaths = [f for f in allPaths - libPaths if isText(f)]
 
-for f in scriptPaths:
-    print(f)
+writeOut(sorted(scriptPaths), outPath)
